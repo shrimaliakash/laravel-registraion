090af6b04a6bb59c697e589b360eda5039afca0e
Akash Shrimali <31008778+shrimaliakash@users.noreply.github.com>
2021-05-15 03:09:29 +0530

    Laravel Registration and Login Using Session

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -21,6 +21,14 @@
                                     @endphp
                                 </div>
                             @endif
+                            @if(Session::has('error'))
+                                <div class="alert alert-danger">
+                                    {{ Session::get('error') }}
+                                    @php
+                                        Session::forget('error');
+                                    @endphp
+                                </div>
+                            @endif
 
 
                         <div class="form-group">
